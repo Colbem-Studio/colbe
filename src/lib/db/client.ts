68f2f4373e78357667de03a69064022396658b62
FlameFragsC <Malebochester@proton.me>
2026-07-28 02:06:45 +0200

    chore: i give up trying

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -1,8 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
 const client = createClient({
-  url: process.env.DATABASE_URL!,
+  url: process.env.DATABASE_URL,
   authToken: process.env.DATABASE_AUTH_TOKEN,
 });
 
