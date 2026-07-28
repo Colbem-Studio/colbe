ea26b535871e513d08857975bc4bfebc0d61e3dd
FlameFragsC <Malebochester@proton.me>
2026-07-28 10:24:39 +0200

    feat: Update files and components date: 28/07/2026

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -25,25 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-        variables: {
-          colorPrimary: "oklch(0.62 0.19 260)",
-          colorBackground: "oklch(0.23 0.004 106)",
-          colorInputText: "oklch(0.97 0 0)",
-          colorText: "oklch(0.97 0 0)",
-          colorTextSecondary: "oklch(0.7 0 0)",
-          borderRadius: "15px",
-        },
-      }}
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <html
-        lang="en"
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
-        <body className="min-h-full flex flex-col">{children}</body>
-      </html>
-    </ClerkProvider>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
   );
 }
