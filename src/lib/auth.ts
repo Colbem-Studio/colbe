a6019c12d435e546ee707873c0d1a1907a511023
FlameFragsC <Malebochester@proton.me>
2026-07-28 10:54:24 +0200

    feat: Authentication inspection

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,4 +11,9 @@ export const auth = betterAuth({
   },
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [
+    "http://localhost:3000",
+    "http://192.168.8.43:3000",
+    "https://colbe.cc",
+  ],
 });
