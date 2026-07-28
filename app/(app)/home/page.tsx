fbfed1882052673452c0dbdcb4875282d7c1e689
FlameFragsC <Malebochester@proton.me>
2026-07-28 11:52:49 +0200

    feat: Upload files for more development purposes

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,7 +1,28 @@
-export default function DashboardPage() {
+import { ServerRail } from "@/components/app/rail/server-rail";
+import { ChannelSidebar } from "@/components/app/sidebar/channel-sidebar";
+import { ProfileBar } from "@/components/app/profile/profile-bar";
+import { Titlebar } from "@/components/app/titlebar/titlebar";
+import { MessageList } from "@/components/app/messages/message-list";
+import { MessageComposer } from "@/components/app/messages/message-composer";
+import { MemberList } from "@/components/app/members/member-list";
+
+export default function HomePage() {
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold text-foreground">Dashboard</h1>
+    <div className="flex h-screen w-full">
+      <ServerRail spaces={[]} />
+
+      <div className="flex flex-col">
+        <ChannelSidebar spaceName="" channels={[]} />
+        <ProfileBar />
+      </div>
+
+      <div className="flex flex-1 flex-col">
+        <Titlebar channelName="" />
+        <MessageList messages={[]} />
+        <MessageComposer channelName="" onSend={() => {}} />
+      </div>
+
+      <MemberList members={[]} />
     </div>
   );
 }
