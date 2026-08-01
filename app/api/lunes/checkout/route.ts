838709540b8f14520c4d488f74075dd04735c2f8
FlameFragsC <Malebochester@proton.me>
2026-08-01 16:58:46 +0200

    feat: add route for lunes api

--- app/api/lunes/checkout/route.ts
+++ app/api/lunes/checkout/route.ts
@@ -1,3 +1,5 @@
-export default function lunesCheckout() {
-  return null;
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  return NextResponse.json({ error: "Not implemented" }, { status: 501 });
 }
